b4e8de0b3e93b373250f01e27efc446cccef3350
kieuduc123 <ducktth2206037@fpt.edu.vn>
2025-02-05 14:06:48 +0700

    updaTE

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        hmr: {
-            overlay: false,
-        },
+  plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false,
     },
-    build: {
-        target: 'esnext',
-        rollupOptions: {
-            input: 'src/main.tsx',
-            output: {
-                format: 'esm',
-            },
-        },
-        minify: false,
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        format: 'esm',
+      },
     },
-    optimizeDeps: {
-        esbuildOptions: {
-            loader: {
-                '.ts': 'tsx',
-            },
-        },
+    minify: false,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.ts': 'tsx',
+      },
     },
+  },
 });
