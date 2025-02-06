5861b273fff20a326eedef7a379d2950c9a0d8b6
kieuduc123 <ducktth2206037@fpt.edu.vn>
2025-02-06 17:47:02 +0700

    update

--- src/App.d.ts
+++
@@ -1,6 +0,0 @@
-import './assets/boxicons-2.1.2/css/boxicons.min.css';
-import 'aos/dist/aos.css';
-import "./App.scss";
-import './scss/_variables.scss';
-declare const App: () => import("react/jsx-runtime").JSX.Element;
-export default App;
--- src/pages/Portfolio/Portfolio.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import './portfolio.scss';
-declare const Portfolio: React.FC;
-export default Portfolio;
